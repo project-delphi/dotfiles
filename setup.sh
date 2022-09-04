8dda9df4474977b2c3edc5022234ad5d2fcef033
Ravi Kalia <ravkalia@gmail.com>
2022-09-04 13:31:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,251 @@
+# modified from Brad Parbs Gist https://gist.github.com/bradp/bea76b16d3325f5c47d4
+echo "Creating an SSH key for you..."
+ssh-keygen -t rsa
+
+echo "Please add this public key to Github \n"
+echo "https://github.com/account/ssh \n"
+read -p "Press [Enter] key after this..."
+
+echo "Installing xcode-stuff"
+xcode-select --install
+
+# Install if we don't have homebrew
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+echo "Updating homebrew..."
+brew update
+
+echo "Installing Git..."
+brew install git
+
+echo "Git config"
+
+git config --global user.name "Ravi Kalia"
+git config --global user.email ravkalia@gmail.com
+
+echo "Installing other brew stuff..."
+
+apps=(oh-my-zsh
+  fortune
+  fzf
+  git
+  htop
+  macchina
+  make
+  mplayer
+  node
+  nodeenv
+  pwgen
+  pyenv
+  pyenv-virtualenv
+  ranger
+  rsync
+  speedtest_cli
+  sl
+  trash
+  tree
+  wget
+  youtube-dl
+)
+brew install ${apps[@]}
+
+echo "Cleaning up brew"
+brew cleanup
+
+echo "Installing homebrew cask"
+brew install caskroom/cask/brew-cask
+
+echo "Making directories"
+mkdir -p $HOME/Projects/repos
+mkdir $HOME/Projects/data
+mkdir $HOME/Desktop/screenshots
+mkdir $HOME/Downloads/incomplete
+
+echo "Copying dotfiles from Github"
+cd ~
+git clone git@github.com:project-delphi/dotfiles.git $HOME/.dotfiles
+cd .dotfiles
+
+
+#Install Zsh & Oh My Zsh
+echo "Installing Oh My ZSH..."
+curl -L http://install.ohmyz.sh | sh
+
+echo "Setting up Oh My Zsh theme..."
+
+echo "Setting up Zsh plugins..."
+cd ~/.oh-my-zsh/custom/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+
+echo "Setting ZSH as shell..."
+chsh -s /bin/zsh
+
+# Apps
+cask_apps=(
+  alfred
+  alt-tab
+  chromecast
+  docker
+  edge
+  firefox
+  google-chrome
+  iterm2
+  onepassword
+  rectangle
+  skype
+  slack
+  spotify
+  transmission
+  visual-studio-code
+  vlc
+  zoomus
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "Installing apps with Cask..."
+brew cask install --appdir="/Applications" ${cask_apps[@]}
+
+brew cask alfred link
+
+brew cleanup
+
+echo "Setting up PyEnv"
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo '"$(pyenv init --path)"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' > ~/.zshrc
+
+echo "Installing Python"
+
+pyenv install 3.10.0
+pyenv global 3.10.0
+
+echo "Setting up Python virtual environments"
+pyenv virtualenv 3.10.0 Projects
+cd $HOME/Projects
+pyenv local Projects 
+cd -
+
+echo "Setting some Mac settings..."
+
+#"Allow text selection in Quick Look"
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+#"Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+#"Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+#"Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+#"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+#"Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+#"Enabling subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+#"Showing icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+#"Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+#"Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+#"Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+#"Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+#"Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+
+#"Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+#"Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+
+#"Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+#"Preventing Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+#"Disable the sudden motion sensor as its not useful for SSDs"
+sudo pmset -a sms 0
+
+#"Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+#"Disable annoying backswipe in Chrome"
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+mkdir $HOME/Desktop/screenshots
+#"Setting screenshots location to ~/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop/screenshots"
+
+#"Setting screenshot format to PNG"
+defaults write com.apple.screencapture type -string "png"
+
+#"Hiding Safari's bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+#"Hiding Safari's sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+#"Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+#"Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+#"Use `~/Downloads/Incomplete` to store incomplete downloads"
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/incomplete"
+
+#"Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+#"Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+#"Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+#"Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+killall Finder
+
+echo "Done!"
 
